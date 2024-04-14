d9a6c5269ce6d6f19f01b8bf2f93dfc02240472d
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-15 10:49:04 +1200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       
+      <h2>Hello World</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi ducimus aperiam doloremque. Veritatis nobis pariatur atque magnam, eligendi enim perferendis tempore iste. Aliquam labore corporis aliquid fugiat! Velit, delectus numquam.</p>
+     </div>
+    </>
+  )
+}
+
+export default App
